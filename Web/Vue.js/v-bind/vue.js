fdbe61c6c93ff8a08b427294221dff26f4610499
kayken7 <75356655+kayken7@users.noreply.github.com>
2020-12-03 00:11:00 +0900

    Create vue.js

---
+++ Web/Vue.js/v-bind/vue.js
@@ -0,0 +1,8 @@
+app = new Vue({
+  el: '#vue-app',
+  data:{
+    name: 'Dr.Smith',
+    website: 'https://www.naver.com',
+    websiteTag: '<a href="https://www.daum.net"> DAUM PORTAL</a>'
+  }
+});
